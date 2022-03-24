040c7014ff9230814fbf4896dd5517256ed65142
Olha Gruzglina <olga.zhmura@gmail.com>
2022-03-24 11:50:59 -0400

    rails g resource for users

---
+++ db/migrate/20220324154919_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+      t.string :password_digest
+      t.string :avatar
+      t.string :hometown
+      t.datetime :birthdate
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
