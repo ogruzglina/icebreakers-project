cd6a9e3535af8b0b7755b20ac167369d243849db
Olha Gruzglina <olga.zhmura@gmail.com>
2022-03-24 13:58:53 -0400

    db migrate and added has_many to models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,4 @@
 class Question < ApplicationRecord
+    has_many :user_answers
+    has_many :users, through: :user_answers
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :user_answers
+    has_many :questions, through: :user_answers
 end
