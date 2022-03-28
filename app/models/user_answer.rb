df66bb4b7a5c547f39612a8aaf10f5e2bfdd641f
Olha Gruzglina <olga.zhmura@gmail.com>
2022-03-28 13:19:04 -0400

    added create route for user_unswer + validations

--- app/models/user_answer.rb
+++ app/models/user_answer.rb
@@ -1,4 +1,8 @@
 class UserAnswer < ApplicationRecord
   belongs_to :user
   belongs_to :question
+
+  validates :answer, presence: true
+  validates :user_id, presence: true, uniqueness: { scope: :question_id, message: "should have one answer per question" }
+  validates :question_id, presence: true
 end
