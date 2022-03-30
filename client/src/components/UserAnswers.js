0a3fe27d7aef31e5facca56346cdb82befa62dd6
Olha Gruzglina <olga.zhmura@gmail.com>
2022-03-30 14:44:06 -0400

    connected logic to current user and added default avatar for sign up

--- client/src/components/UserAnswers.js
+++ client/src/components/UserAnswers.js
@@ -7,7 +7,7 @@ function UserAnswers({ uAnswers, errorFound }) {
             const answers = uAnswers.map( a => 
                 <div key = { a.id }> 
                     <img 
-                        src = { a.user.avatar ? a.user.avatar : "https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=20&m=1223671392&s=612x612&w=0&h=lGpj2vWAI3WUT1JeJWm1PRoHT3V15_1pdcTn2szdwQ0=" } 
+                        src = { a.user.avatar } 
                         alt = "user-avatar" 
                         style = {{ height: "50px", width: "50px", borderRadius: "50px" }}
                     />  
